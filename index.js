00fa1d25de4bf3f2fe41d2cd58b8c884b5a85e61
Bret Comnes <bcomnes@gmail.com>
2022-06-11 11:08:02 -0600

    Try agaib

--- index.js
+++ index.js
@@ -47,7 +47,7 @@ fastify.get('/redirect-chain', {
  */
 const start = async () => {
   try {
-    await fastify.listen({ port: process.env.PORT ?? 3000 })
+    await fastify.listen({ port: process.env.PORT || 3000 })
   } catch (err) {
     fastify.log.error(err)
     process.exit(1)
